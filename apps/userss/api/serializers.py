059bccd14688868d9efe067be636fb20adfb10ba
Carlos Blandon <blandoncarlos265@gmail.com>
2022-09-29 16:13:54 -0500

    AEncrypt password to methods create and edit

--- apps/userss/api/serializers.py
+++ apps/userss/api/serializers.py
@@ -7,6 +7,9 @@ class UserstokenSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=('username','name','password')
+    
+    
+
 
 
 
@@ -14,3 +17,15 @@ class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields='__all__'
+
+    def create(self, validated_data):
+        user=User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+    
+    def update(self, instance, validated_data):
+        updated_user= super().update(instance, validated_data)
+        updated_user.set_password(validated_data['password'])
+        updated_user.save()
+        return updated_user
