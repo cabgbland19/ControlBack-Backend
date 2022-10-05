f36aad99c25e09d55842b83e5eddc0e0a076ae69
Carlos Blandon <blandoncarlos265@gmail.com>
2022-10-05 12:01:12 -0500

    Add configuration to connection with sqlserver

--- controlgeneral/settings/local.py
+++ controlgeneral/settings/local.py
@@ -4,14 +4,27 @@ DEBUG = True
 ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'controlback',
-        'USER': 'root',
+        'ENGINE': 'mssql',
+        'NAME':'control_back',
+        'USER': '',
         'PASSWORD':'',
-        'HOST': 'localhost',
-        'PORT':'3306'
+        'HOST': 'U27TEC01\SQLEXPRESS',
+        'PORT':'',
+        'OPTIONS':{
+            "driver":'ODBC Driver 17 for SQL Server'
+        },
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME':'controlback',
+#         'USER': 'root',
+#         'PASSWORD':'',
+#         'HOST': 'localhost',
+#         'PORT':'3306'
+#     }
+# }
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
